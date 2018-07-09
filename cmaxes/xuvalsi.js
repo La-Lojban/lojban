656bf875b8db8f209dcbe384bbe3bfac41c0d313
lagleki <gleki.is.my.name@gmail.com>
2018-07-09 11:50:52 -0700

    fix to "xuvalsi" function

--- cmaxes/xuvalsi.js
+++ cmaxes/xuvalsi.js
@@ -1,8 +1,21 @@
-const xuvalsi = (str,type,cmaxes) => {
-  try{return cmaxes(str)["te spuda"][0][0]===type;}catch(e){return false;}
+const xuvalsi = (str, type, cmaxes) => {
+  try {
+    const c = cmaxes(str)["te spuda"];
+    return c.length === 1 && c[0][0] === type;
+  } catch (e) {
+    return false;
+  }
 }
 
-const xugismu = (str,cmaxes) => {return xuvalsi(str, "gismu", cmaxes);}
-const xulujvo = (str,cmaxes) => {return xuvalsi(str, "lujvo", cmaxes);}
+const xugismu = (str, cmaxes) => {
+  return xuvalsi(str, "gismu", cmaxes);
+}
+const xulujvo = (str, cmaxes) => {
+  return xuvalsi(str, "lujvo", cmaxes);
+}
 
-module.exports = {xuvalsi, xugismu, xulujvo}
+module.exports = {
+  xuvalsi,
+  xugismu,
+  xulujvo
+}
